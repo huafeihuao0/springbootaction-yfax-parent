56277ae8390668103514f93823f9888b13c27974
minbo <hemin_it@163.com>
2017-08-24 17:10:46 +0800

    update

--- yfax-qmtt-api/src/main/java/com/yfax/webapi/qmtt/vo/IncomeSetVo.java
+++ yfax-qmtt-api/src/main/java/com/yfax/webapi/qmtt/vo/IncomeSetVo.java
@@ -9,11 +9,18 @@ public class IncomeSetVo implements Serializable{
 	private String id;			//主键
 	private String income;		//提现金额
 	private Integer status;		//记录状态，1=已下线，2=已上线
+	private Integer saleFlag;	//销售标识，1=下线；2=上线；3=缺货
 	private String createDate;	//创建时间
 	private String createdBy;	//创建人
 	private String updateDate;	//更新时间
 	private String updatedBy;	//更新人
 	
+	public Integer getSaleFlag() {
+		return saleFlag;
+	}
+	public void setSaleFlag(Integer saleFlag) {
+		this.saleFlag = saleFlag;
+	}
 	public String getId() {
 		return id;
 	}
