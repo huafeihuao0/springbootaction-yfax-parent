6789e44c915a581476e6bc129245d63bd2bbf751
minbo <hemin_it@163.com>
2017-09-04 10:36:47 +0800

    update

--- yfax-webapi/src/main/java/com/yfax/webapi/oauth/OAuth2Configuration.java
+++ yfax-webapi/src/main/java/com/yfax/webapi/oauth/OAuth2Configuration.java
@@ -98,7 +98,7 @@ public class OAuth2Configuration {
                     .authorities(Authorities.ROLE_ADMIN.name(), Authorities.ROLE_USER.name())
                     .authorizedGrantTypes("password", "refresh_token")
                     .secret(propertyResolver.getProperty(PROP_SECRET))
-                    .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1296000));
+                    .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 120));
         }
 
         @Override
