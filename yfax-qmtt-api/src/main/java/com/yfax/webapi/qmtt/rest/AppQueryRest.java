499ccea264cd34eb6433ef2ef6756134a27a3b2f
minbo <hemin_it@163.com>
2017-09-13 16:33:38 +0800

    update

--- yfax-qmtt-api/src/main/java/com/yfax/webapi/qmtt/rest/AppQueryRest.java
+++ yfax-qmtt-api/src/main/java/com/yfax/webapi/qmtt/rest/AppQueryRest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -219,13 +220,13 @@ public class AppQueryRest {
 		return dFormat.format(sum);
 	}
 	
-	public static void main(String[] args) {
-		AppQueryRest rest = new AppQueryRest();
-		List<AppUserVo> list = rest.initTestData(new ArrayList<AppUserVo>());
-		for (AppUserVo appUserVo : list) {
-			System.out.println(appUserVo);
-		}
-	}
+//	public static void main(String[] args) {
+//		AppQueryRest rest = new AppQueryRest();
+//		List<AppUserVo> list = rest.initTestDataGold(new ArrayList<AppUserVo>());
+//		for (AppUserVo appUserVo : list) {
+//			System.out.println(appUserVo);
+//		}
+//	}
 	
 	//测试方法，虚拟数据
 	private List<AppUserVo> initTestData(List<AppUserVo> list) {
@@ -240,7 +241,7 @@ public class AppQueryRest {
 		int salt = 10;
 		//格式化，保留三位小数，DB做四舍五入
 		DecimalFormat dFormat = new DecimalFormat(GlobalUtils.DECIMAL_FORMAT); 
-		for (int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < 3; i++) {
 			AppUserVo appUserVo = new AppUserVo();
 			appUserVo.setPhoneNum(strings[i]);
 			appUserVo.setBalance(String.valueOf(dFormat.format(new Random().nextInt(100) * salt * new Random().nextFloat())));
@@ -267,11 +268,22 @@ public class AppQueryRest {
 				,"13500002325","15600006843","15800001335","13000008731","18000007232","18800002545","18200006813","13900001314"
 				,"15900005632","13300008683","18400001332","13500002338","15600006682","15800001334","13000004153","18000009773"
 				,"18800009377","18200001733","13900001632"};
-		for (int i = 0; i < strings.length; i++) {
+		for (int i = 0; i < 3; i++) {
 			AppUserVo appUserVo = new AppUserVo();
 			appUserVo.setPhoneNum(strings[i]);
-			DecimalFormat myFormat = new DecimalFormat("#0");
-			appUserVo.setGold(myFormat.format(Double.valueOf(new Random().nextFloat() * 1000000 * new Random().nextFloat())));
+			appUserVo.setGold(String.valueOf(getValue(1)));
+			list.add(appUserVo);
+		}
+		for (int i = 3; i < 11; i++) {
+			AppUserVo appUserVo = new AppUserVo();
+			appUserVo.setPhoneNum(strings[i]);
+			appUserVo.setGold(String.valueOf(getValue(2)));
+			list.add(appUserVo);
+		}
+		for (int i = 11; i < 21; i++) {
+			AppUserVo appUserVo = new AppUserVo();
+			appUserVo.setPhoneNum(strings[i]);
+			appUserVo.setGold(String.valueOf(getValue(3)));
 			list.add(appUserVo);
 		}
 		Collections.sort(list, new Comparator<AppUserVo>() {
@@ -297,4 +309,101 @@ public class AppQueryRest {
 		return new JsonResult(ResultCode.SUCCESS, initConfigVo);
 	}
 	
+//	public static void main(String[] args) {
+//		
+//		DecimalFormat myFormat = new DecimalFormat("#0");
+//		System.out.println("1=" + getValue(1));
+//		System.out.println("2=" + getValue(1));
+//		System.out.println("3=" + getValue(1));
+//		System.out.println();
+//		System.out.println("4=" + getValue(2));
+//		System.out.println("5=" + getValue(2));
+//		System.out.println("6=" + getValue(2));
+//		System.out.println("7=" + getValue(2));
+//		System.out.println("8=" + getValue(2));
+//		System.out.println("9=" + getValue(2));
+//		System.out.println("10=" + getValue(2));
+//		System.out.println();
+//		System.out.println("11=" + getValue(3));
+//		System.out.println("12=" + getValue(3));
+//		System.out.println("13=" + getValue(3));
+//		System.out.println("14=" + getValue(3));
+//		System.out.println("15=" + getValue(3));
+//		System.out.println("16=" + getValue(3));
+//		System.out.println("17=" + getValue(3));
+//		System.out.println("18=" + getValue(3));
+//		System.out.println("19=" + getValue(3));
+//		System.out.println("20=" + getValue(3));
+//		
+//		System.exit(1);
+//		
+//		List<AppUserVo> list = new ArrayList<>();
+//		
+//		String[] strings = new String[] {"15900008732","13300008123","18400001351","13500002314","15600006532","15800001317"
+//				,"13000004123","18000009873","18800009345","18200001713","13900001532","15900002017","13300001789","18400001638"
+//				,"13500002325","15600006843","15800001335","13000008731","18000007232","18800002545","18200006813","13900001314"
+//				,"15900005632","13300008683","18400001332","13500002338","15600006682","15800001334","13000004153","18000009773"
+//				,"18800009377","18200001733","13900001632"};
+//		for (int i = 0; i < 3; i++) {
+//			AppUserVo appUserVo = new AppUserVo();
+//			appUserVo.setPhoneNum(strings[i]);
+////			DecimalFormat myFormat = new DecimalFormat("#0");
+//			appUserVo.setGold(myFormat.format(Double.valueOf(new Random().nextDouble() * 100000000 * new Random().nextFloat())));
+//			list.add(appUserVo);
+//		}
+////		for (int i = 3; i < 11; i++) {
+////			AppUserVo appUserVo = new AppUserVo();
+////			appUserVo.setPhoneNum(strings[i]);
+////			DecimalFormat myFormat = new DecimalFormat("#0");
+////			appUserVo.setGold(myFormat.format(Double.valueOf(new Random().nextFloat() * 1000000 * new Random().nextFloat())));
+////			list.add(appUserVo);
+////		}
+////		for (int i = 11; i < strings.length; i++) {
+////			AppUserVo appUserVo = new AppUserVo();
+////			appUserVo.setPhoneNum(strings[i]);
+////			DecimalFormat myFormat = new DecimalFormat("#0");
+////			appUserVo.setGold(myFormat.format(Double.valueOf(new Random().nextFloat() * 1000000 * new Random().nextFloat())));
+////			list.add(appUserVo);
+////		}
+//		Collections.sort(list, new Comparator<AppUserVo>() {
+//	        @Override
+//	        public int compare(AppUserVo o1, AppUserVo o2) {
+//	        		int result = Integer.valueOf(o2.getGold()) - Integer.valueOf(o1.getGold());
+//	        		return result;
+//	        }
+//	    });
+//		
+//		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+//			AppUserVo appUserVo = (AppUserVo) iterator.next();
+//			System.out.println(appUserVo.toString());
+//		}
+//	}
+	
+	public static Integer getValue(int type) {
+		DecimalFormat myFormat = new DecimalFormat("#0");
+		Integer result = 0;
+		if(type == 1) {
+			while(true) {
+				result = Integer.valueOf(myFormat.format(new Random().nextFloat() * 100000000 * new Random().nextFloat())) * 2;
+				if(result>5000*10000 && result<10000*10000) {
+					return result;
+				}
+			}
+		}else if(type == 2) {
+			while(true) {
+				result = Integer.valueOf(myFormat.format(new Random().nextFloat() * 100000000 * new Random().nextFloat()));
+				if(result>2000*10000 && result<5000*10000) {
+					return result;
+				}
+			}
+		}else if(type == 3) {
+			while(true) {
+				result = Integer.valueOf(myFormat.format(new Random().nextFloat() * 100000000 * new Random().nextFloat()));
+				if(result>500*10000 && result<2000*10000) {
+					return result;
+				}
+			}
+		}
+		return result;
+	}
 }
