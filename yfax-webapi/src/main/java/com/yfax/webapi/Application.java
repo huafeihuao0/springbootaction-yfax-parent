1527e2874172d36dc41ccf3b454030aa1e8ff08a
huafeihuao0 <yx16395175@163.com>
2018-03-31 08:50:34 +0800

    重构SB应用入口和配置

--- yfax-webapi/src/main/java/com/yfax/webapi/Application.java
+++ yfax-webapi/src/main/java/com/yfax/webapi/Application.java
@@ -5,16 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * @author Minbo.He
- */
-@SpringBootApplication
-@EnableTransactionManagement
-@EnableAsync(proxyTargetClass=true)
-public class Application{
-	
-	// 服务启动入口
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+
+/***
+ *  【SB应用入口和配置】
+ * */
+@SpringBootApplication  //SB app
+@EnableTransactionManagement    // 开启自动事务管理
+@EnableAsync(proxyTargetClass = true)   //开启异步支持
+public class Application
+{
+    // 服务启动入口
+    public static void main(String[] args)
+    {
+        //开启运行
+        SpringApplication.run(Application.class, args);
+    }
 }
