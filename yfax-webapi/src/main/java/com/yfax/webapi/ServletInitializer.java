d0dcde81fe23e71b089e850fe70b17defd4eb713
huafeihuao0 <yx16395175@163.com>
2018-03-31 08:47:55 +0800

    重构springboot servlet 初始化器

--- yfax-webapi/src/main/java/com/yfax/webapi/ServletInitializer.java
+++ yfax-webapi/src/main/java/com/yfax/webapi/ServletInitializer.java
@@ -3,11 +3,21 @@ package com.yfax.webapi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+/***
+ *  【springboot servlet 初始化器】
+ * */
+public class ServletInitializer
+        extends SpringBootServletInitializer//SB servlet初始化器
+{
+
+    /***
+     *  加载应用配置
+     * */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springAppBuilder)
+    {
+        return springAppBuilder.sources(Application.class);
+    }
 
 }
